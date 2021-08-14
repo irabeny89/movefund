499cca328cfa82d5398e31fbdddd68e7c0ecad44
Ernest Irabor <irabeny89@gmail.com>
2021-08-15 00:59:11 +0100

    refactor

--- utils/index.ts
+++ utils/index.ts
@@ -2,7 +2,6 @@ import { randomBytes, pbkdf2 } from "crypto";
 import { promisify } from "util";
 import { sign } from "jsonwebtoken";
 import { Model } from "mongoose";
-import DataLoader from "dataloader";
 import { AuthenticationError, ValidationError } from "apollo-server-micro";
 import {
   LoginInputType,
@@ -10,19 +9,18 @@ import {
   UserCredentialType,
   AuthUserType,
   UserType,
-  GraphContextType,
 } from "../types";
 import UserModel from "../models/modelUser";
 
-export const getSalt = () => randomBytes(32).toString("hex");
+const getSalt = () => randomBytes(32).toString("hex");
 
-export const hashPassword = async (password: string, salt: string) => {
+const hashPassword = async (password: string, salt: string) => {
   const asyncPbkdf2 = promisify(pbkdf2);
   const hash = await asyncPbkdf2(password, salt, 50000, 64, "sha512");
   return hash.toString("hex");
 };
 
-export const verifyPassword = async (
+const verifyPassword = async (
   password: string,
   hashedPassword: string,
   salt: string
@@ -31,7 +29,7 @@ export const verifyPassword = async (
   return hash === hashedPassword;
 };
 
-export const generateToken = ({
+const generateToken = ({
   secret,
   audience,
   issuer,
@@ -43,66 +41,28 @@ export const generateToken = ({
     issuer,
   });
 
-export const validatePassword = (password: string) => {
+const validatePassword = (password: string) => {
   if (!password.length) throw new ValidationError("Please enter password");
   if (password.length < 6)
     throw new ValidationError("Please make password longer than 5 characters");
 };
 
-export const register = async ({ password, ...rest }: UserCredentialType) => {
-  const accessTokenSecret = process.env.JWT_ACCESS_SECRET!;
-  const refreshTokenSecret = process.env.JWT_REFRESH_SECRET!;
-  const issuer = process.env.TOKEN_ISSUER || "http://localhost:3000";
+const getAuth = ({
+  isAdmin,
+  _id,
+  firstname,
+}: {
+  isAdmin: boolean;
+  firstname: string;
+  _id: string;
+}): {
+  accessToken: string;
+  refreshToken: string;
+  isAdmin: boolean;
+  firstname: string;
+  _id: string;
+} => {
   try {
-    validatePassword(password);
-    const salt = getSalt();
-    const hashedPassword = await hashPassword(password, salt);
-    const { isAdmin, firstname, _id } = await UserModel.create({
-      salt,
-      hashedPassword,
-      ...rest,
-    });
-    const userScope = isAdmin
-      ? process.env.ADMIN_SCOPE!
-      : process.env.USER_SCOPE!;
-    const accessToken = generateToken({
-      secret: accessTokenSecret,
-      audience: userScope,
-      firstname,
-      _id,
-      issuer,
-    });
-    const refreshToken = generateToken({
-      secret: refreshTokenSecret,
-      audience: userScope,
-      firstname,
-      _id,
-      issuer,
-    });
-    return {
-      firstname,
-      _id,
-      isAdmin,
-      accessToken,
-      refreshToken,
-    };
-  } catch (err) {
-    throw err;
-  }
-};
-
-export const logUserIn = async (
-  { email, password }: LoginInputType,
-  UserModel: Model<UserType>
-): Promise<AuthUserType | undefined> => {
-  try {
-    const user = await UserModel.findOne({ email })
-      .select("_id firstname isAdmin hashedPassword salt")
-      .exec();
-    if (!user) throw new AuthenticationError("User not found!");
-    if ((await hashPassword(password, user.salt)) !== user.hashedPassword)
-      throw new AuthenticationError("Wrong inputs!");
-    const { _id, firstname, isAdmin } = user;
     const accessTokenSecret = process.env.JWT_ACCESS_SECRET!;
     const refreshTokenSecret = process.env.JWT_REFRESH_SECRET!;
     const issuer = process.env.TOKEN_ISSUER || "http://localhost:3000";
@@ -130,6 +90,40 @@ export const logUserIn = async (
       accessToken,
       refreshToken,
     };
+  } catch (err) {
+    throw new Error("Something went wrong while authenticating.");
+  }
+};
+
+export const register = async ({ password, ...rest }: UserCredentialType) => {
+  try {
+    validatePassword(password);
+    const salt = getSalt();
+    const hashedPassword = await hashPassword(password, salt);
+    const { isAdmin, firstname, _id } = await UserModel.create({
+      salt,
+      hashedPassword,
+      ...rest,
+    });
+    return getAuth({ isAdmin, firstname, _id });
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const logUserIn = async (
+  { email, password }: LoginInputType,
+  UserModel: Model<UserType>
+): Promise<AuthUserType | undefined> => {
+  try {
+    const user = await UserModel.findOne({ email })
+      .select("_id firstname isAdmin hashedPassword salt")
+      .exec();
+    if (!user) throw new AuthenticationError("User not found!");
+    if ((await hashPassword(password, user.salt)) !== user.hashedPassword)
+      throw new AuthenticationError("Wrong inputs!");
+    const { _id, firstname, isAdmin } = user;
+    return getAuth({ _id, firstname, isAdmin });
   } catch (error) {
     throw error;
   }
