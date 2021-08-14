32e95c90ed74fb38701093c6f59b9915df740fef
Ernest Irabor <irabeny89@gmail.com>
2021-08-14 21:49:15 +0100

    3 document loaders

---
+++ dataloaders/moneyOutLoader.ts
@@ -0,0 +1,7 @@
+import Dataloader from "dataloader";
+import MoneyOutModel from "../models/modelMoneyOut"
+
+const getMoneyOutLoader = () =>
+  new Dataloader((ids: any) => MoneyOutModel.find({ _id: { $in: ids } }).exec());
+
+export default getMoneyOutLoader
