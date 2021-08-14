048fbb0f1f49465385770ed9648cf008820a5d89
Ernest Irabor <irabeny89@gmail.com>
2021-08-14 21:50:02 +0100

    register mutation

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,7 +1,22 @@
+import { UserInputError, ValidationError } from "apollo-server-micro";
+import type { AuthUserType, GraphContextType } from "../types";
+import { register } from "../utils";
 
 const resolvers = {
   Query: {
-    hello: () => "world!"
+    hello: () => "world!",
+  },
+  Mutation: {
+    register: async (
+      _: any,
+      { credential }: any
+    ): Promise<AuthUserType | undefined> => {
+      try {
+        return await register(credential);
+      } catch (err: any) {
+        throw err;
+      }
+    },
   },
 };
 
