0c129e8dbdc9eccccbc38cbc645bfcb92af22328
Ernest Irabor <irabeny89@gmail.com>
2021-08-18 19:04:32 +0100

    database function refactor

--- models/index.ts
+++ models/index.ts
@@ -1,18 +1,15 @@
-import { connections, createConnection } from "mongoose";
+import { connect, connections } from "mongoose";
 
-const dbConnection = () => {
+const dbConnection = async () => {
   try {
-    if (!connections[0]) {
-      const db = createConnection(process.env.DB_URL!, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-      });
-      console.log("Database: %s", process.env.DB_URL);
-      return db
-    }
-    console.log("Database still on: %s", process.env.DB_URL);
-    return connections[0]
+    // if (connections[0]) return 
+    await connect(process.env.DB_URL!, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      autoIndex: false,
+    });
+    console.log("Database connected: %s", process.env.DB_URL)
+    // await connections[0].close()
   } catch (err) {
     console.log(err);
   }
