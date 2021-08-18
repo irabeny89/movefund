327cc232fb2485627ed8a0e9ac76a5981e9897eb
Ernest Irabor <irabeny89@gmail.com>
2021-08-18 11:13:01 +0100

    user resolver crud added

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,36 +1,72 @@
-import type {
-  AuthUserType,
-  GraphContextType,
-  LoginInputType,
-  UserCredentialType,
-} from "../types";
-import { logUserIn, register } from "../utils";
+import type { GraphContextType, UserType } from "types";
+import { ValidationError, UserInputError } from "apollo-server-micro";
+
+const mutationResponse = "Completed";
 
 const resolvers = {
   Query: {
     hello: () => "world!",
+    getUser: async (
+      _: any,
+      { id }: { id: string },
+      { users }: GraphContextType
+    ): Promise<UserType> => {
+      try {
+        return await users.load(id);
+      } catch (error) {
+        throw new UserInputError("ID not existing");
+      }
+    },
+    getAllUsers: async (
+      _: any,
+      __: any,
+      { UserModel }: GraphContextType
+    ): Promise<UserType[]> => await UserModel.find({}),
   },
   Mutation: {
     register: async (
       _: any,
-      { registerInput }: { registerInput: UserCredentialType }
-    ): Promise<AuthUserType | undefined> => {
-      try {
-        return await register(registerInput);
-      } catch (err: any) {
-        throw err;
-      }
-    },
-    login: async (
-      _: any,
-      { loginInput }: { loginInput: LoginInputType },
+      { userInfo }: { userInfo: UserType },
       { UserModel }: GraphContextType
-    ): Promise<AuthUserType | undefined> => {
-      try {
-        return logUserIn(loginInput, UserModel);
-      } catch (err) {
-        throw err;
-      }
+    ): Promise<UserType> => {
+      const isExisting = await UserModel.findOne({ email: userInfo.email });
+      if (isExisting) throw new ValidationError("Already existing");
+      return await UserModel.create(userInfo);
+    },
+    updateUser: async (
+      _: any,
+      {
+        id,
+        userUpdate,
+      }: {
+        id: string;
+        userUpdate: Pick<
+          UserType,
+          | "avatar"
+          | "street"
+          | "localGovernmentArea"
+          | "city"
+          | "state"
+          | "country"
+        >;
+      },
+      { UserModel }: GraphContextType
+    ) => {
+      const isExisting = await UserModel.findByIdAndUpdate(
+        id,
+        userUpdate
+      ).exec();
+      if (!isExisting) throw new UserInputError("ID not existing");
+      return isExisting;
+    },
+    removeUser: async (
+      _: any,
+      { id }: { id: string },
+      { UserModel }: GraphContextType
+    ): Promise<string> => {
+      const isExisting = await UserModel.findByIdAndDelete(id).exec();
+      if (!isExisting) throw new UserInputError("ID not existing");
+      return mutationResponse;
     },
   },
 };
