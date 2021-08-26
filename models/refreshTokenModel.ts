aa23650b54cccf9bab2013fdc26a1d12f94044f9
Ernest Irabor <irabeny89@gmail.com>
2021-08-26 13:23:26 +0100

    token model initial commit

---
+++ models/refreshTokenModel.ts
@@ -0,0 +1,12 @@
+import { models, Schema, model, Model } from "mongoose";
+import type { RefreshTokenType } from "types";
+
+const schema = new Schema<RefreshTokenType>(
+  {
+    token: { type: String, unique: true }
+  },
+  { timestamps: true }
+);
+
+export default (models.RefreshToken as Model<RefreshTokenType, {}, {}>) ||
+  model<RefreshTokenType>("RefreshToken", schema);
