8ca8e451572cf14ed6162d7e9df2ccaf00499a34
Ernest Irabor <irabeny89@gmail.com>
2021-08-27 10:34:13 +0100

    path aliased

--- graphql/index.ts
+++ graphql/index.ts
@@ -3,14 +3,22 @@ import {
   ApolloServerPluginLandingPageGraphQLPlayground,
   ApolloServerPluginLandingPageDisabled,
 } from "apollo-server-core";
-import typeDefs from "./typeDefs";
-import resolvers from "./resolvers";
+import typeDefs from "@/graphql/typeDefs";
+import resolvers from "@/graphql/resolvers";
 import type { GraphContextType, ContextArgType } from "types";
-import getUserLoader from "./dataloaders/userLoader";
-import getMoneyInLoader from "./dataloaders/moneyInLoader";
-import getMoneyOutLoader from "./dataloaders/moneyOutLoader";
-import UserModel from "../models/modelUser";
-import dbConnection from "../models";
+import getUserLoader from "@/graphql/dataloaders/userLoader";
+import getTransferOutLoader from "@/graphql/dataloaders/transferOutLoader";
+import getTransferInLoader from "@/graphql/dataloaders/transferInLoader";
+import getWithdrawalLoader from "@/graphql/dataloaders/withdrawalLoader";
+import getLoanLoader from "@/graphql/dataloaders/loanLoader";
+import UserModel from "@/models/userModel";
+import TransferInModel from "@/models/transferInModel";
+import TransferOutModel from "@/models/transferOutModel";
+import WithdrawalModel from "@/models/withdrawalModel";
+import LoanModel from "@/models/loanModel";
+import SelfTransferModel from "@/models/selfTransferModel";
+import RefreshTokenModel from "@/models/refreshTokenModel";
+import dbConnection from "@/models/index";
 
 const apolloServer = new ApolloServer({
   typeDefs,
@@ -21,14 +29,22 @@ const apolloServer = new ApolloServer({
       : ApolloServerPluginLandingPageGraphQLPlayground(),
   ],
   context: async ({ req, res }: ContextArgType): Promise<GraphContextType> => {
-    await dbConnection()
+    await dbConnection();
     return {
       req,
       res,
       users: getUserLoader(),
-      moneyIns: getMoneyInLoader(),
-      moneyOuts: getMoneyOutLoader(),
+      transfersIn: getTransferInLoader(),
+      transfersOut: getTransferOutLoader(),
+      withdrawals: getWithdrawalLoader(),
+      loans: getLoanLoader(),
       UserModel,
+      TransferInModel,
+      TransferOutModel,
+      WithdrawalModel,
+      LoanModel,
+      SelfTransferModel,
+      RefreshTokenModel,
     };
   },
 });
