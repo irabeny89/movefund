462617e74ddbbd0764be34cb0a99e245fc7b0bc8
Ernest Irabor <irabeny89@gmail.com>
2021-08-27 10:37:42 +0100

    add debug

--- models/index.ts
+++ models/index.ts
@@ -1,15 +1,14 @@
-import { connect, connections } from "mongoose";
+import mongoose, { connect } from "mongoose";
 
 const dbConnection = async () => {
   try {
-    // if (connections[0]) return 
+    mongoose.set("debug", true)
     await connect(process.env.DB_URL!, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       autoIndex: false,
     });
     console.log("Database connected: %s", process.env.DB_URL)
-    // await connections[0].close()
   } catch (err) {
     console.log(err);
   }
