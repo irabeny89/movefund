086b494ab1aaf2ad97974b6f6fede1805a018a2a
Ernest Irabor <irabeny89@gmail.com>
2021-09-05 17:06:05 +0100

    database connection

--- models/index.ts
+++ models/index.ts
@@ -2,13 +2,14 @@ import mongoose, { connect } from "mongoose";
 
 const dbConnection = async () => {
   try {
-    mongoose.set("debug", true)
+    mongoose.set("debug", true);
     await connect(process.env.DB_URL!, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       autoIndex: false,
+      useFindAndModify: false,
     });
-    console.log("Database connected: %s", process.env.DB_URL)
+    console.log("Database connected: %s", process.env.DB_URL);
   } catch (err) {
     console.log(err);
   }
