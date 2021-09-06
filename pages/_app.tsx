3c04e6333e665f9d649ab6667950731d3cf756b5
Ernest Irabor <irabeny89@gmail.com>
2021-09-06 17:57:13 +0100

    apollo client setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "lib/apolloClient";
 import type { AppProps } from "next/app";
 import ErrorBoundary from "@/components/ErrorBoundary";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps)
   return (
     <ErrorBoundary>
+      <ApolloProvider client={apolloClient}>
       <Component {...pageProps} />
+      </ApolloProvider>
     </ErrorBoundary>
   );
 }
