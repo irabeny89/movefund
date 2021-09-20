9fea7bc5d010618d9467910c08fdbc7050263019
Ernest Irabor <irabeny89@gmail.com>
2021-09-20 15:13:03 +0100

    enabled auth header

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -9,14 +9,14 @@ const handler = async (req: MicroRequest, res: NextApiResponse) => {
   //   "Access-Control-Allow-Origin",
   //   "https://studio.apollographql.com"
   // );
-  // res.setHeader(
-  //   "Access-Control-Allow-Headers",
-  //   "Origin, X-Requested-With, Content-Type, Accept"
-  // );
-  // if (req.method === "OPTIONS") {
-  //   res.end();
-  //   return false;
-  // }
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  if (req.method === "OPTIONS") {
+    res.end();
+    return false;
+  }
   await server;
   await apolloServer.createHandler({ path: "/api/graphql" })(req, res);
 };
